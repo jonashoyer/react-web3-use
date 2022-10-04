fd4082405db1525733638f1b0101327f061d5281
jonashoyer <12399677+jonashoyer@users.noreply.github.com>
2022-10-04 20:14:45 +0200

    Added network to context

--- src/useWeb3UseContext.tsx
+++ src/useWeb3UseContext.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Web3Provider, JsonRpcSigner } from '@ethersproject/providers';
+import { Web3Provider, JsonRpcSigner, Network } from '@ethersproject/providers';
 
 export type Web3UseContextValue = {
   provider: Web3Provider | undefined;
   signer: JsonRpcSigner | undefined;
+  network: Network | undefined;
 }
 
 export const Web3UseContext = React.createContext<Web3UseContextValue>({
   provider: undefined,
   signer: undefined,
+  network: undefined,
 });
 
 export function useWeb3UseContext() {
@@ -19,8 +21,28 @@ export const Web3UseContextProvider: React.FC<{ children: React.ReactNode, getPr
 
   const provider = getProvider();
 
+  const [network, setNetwork] = React.useState<Network>();
+
   const signer = React.useMemo(() => {
     return provider?.getUncheckedSigner();
+  }, [provider]);
+
+  React.useEffect(() => {
+    if (!provider) return;
+
+    const onNetworkChange = (network: Network) => {
+      setNetwork(network);
+    }
+
+    provider.on('network', onNetworkChange);
+
+    return () => {
+      provider.off('network', onNetworkChange);
+    }
+  }, [provider]);
+
+  React.useEffect(() => {
+    provider?.getNetwork().then(network => setNetwork(network));
   }, [provider]);
 
 
@@ -29,6 +51,7 @@ export const Web3UseContextProvider: React.FC<{ children: React.ReactNode, getPr
       value={{
         signer,
         provider,
+        network,
       }}
     >
       {children}
