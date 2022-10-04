58ae22cbcceaa6e5c767f7a485c22076a47468b1
jonashoyer <12399677+jonashoyer@users.noreply.github.com>
2022-10-04 17:42:21 +0200

    Added useNativeBalance

---
+++ src/Native/useNativeBalance.tsx
@@ -0,0 +1,32 @@
+import { useWeb3UseContext } from '../useWeb3UseContext';
+import { useAsyncRetry } from 'react-use';
+
+export type UseNativeBalanceOptions = {
+  accountAddress?: string;
+}
+
+export const useNativeBalance = ({ accountAddress }: UseNativeBalanceOptions) => {
+
+  const { provider, signer } = useWeb3UseContext();
+
+  const { retry: refetch, ...state } = useAsyncRetry(async () => {
+
+    if ((!accountAddress && !signer) || (!provider && accountAddress)) {
+      return undefined;
+    }
+
+
+    if (accountAddress) {
+      return provider!.getBalance(accountAddress);
+    }
+
+    return signer!.getBalance();
+  },
+    [accountAddress, signer, provider],
+  );
+
+  return {
+    ...state,
+    refetch,
+  }
+}
